e37b542c49cae07c84365f4ca9575d3f75e3130f
drodr211 <drodr211@ucr.edu>
2023-07-17 00:14:12 -0700

    new tickrate changes

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 import pygame
 pygame.init()
 
-w = 600
-h = 900
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 900
+tickrate = 120
+tick = 0
 x = 100
 y = 100
 l = 40
-screen = pygame.display.set_mode((w, h))
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 font = pygame.font.Font(pygame.font.get_default_font(), 36)
 
-tick = 0
+
 
 run = True
 while run: 
 
-    pygame.time.Clock().tick(60)
+    clock.tick(tickrate)
     screen.fill((204, 204, 204))
-    
-    tick+=1
-    if tick == 60: 
-        y= y+l
-        tick = 0
-    
-    pygame.draw.rect(screen, (0, 50, 100), (x, y, l, l))
     pygame.draw.line(screen, (0,0,0), (100,100), (100, 860) )
     pygame.draw.line(screen, (0,0,0), (500,100), (500, 860) )
     pygame.draw.line(screen, (0,0,0), (100, 860), (500, 860) )
     pygame.draw.line(screen, (0,0,0), (100,100), (500, 100) )
-    text_surface = font.render('X:'+str(x)+"Y:"+str(y), True, (0, 0, 0))
+    
+    tick+=1
+    if tick == tickrate/2: 
+        y = y+l
+        tick = 0
+
+    pygame.draw.rect(screen, (0, 50, 100), (x, y, l, l))
+    text_surface = font.render('X:'+str(x)+" Y:"+str(y)+" FPS:"+str(round(clock.get_fps())), True, (0, 0, 0))
 
 
     for event in pygame.event.get():
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT and x>100:
-                x -= 20
+                x -= l
             elif event.key==pygame.K_RIGHT and x+l<500:
-                x += 20
+                x += l
         if event.type == pygame.QUIT:
             run = False
     
