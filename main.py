3985c210894754760687baef536b956ccd0a3d88
Daniel Rodriguez <drodr211@ucr.edu>
2023-07-17 10:51:02 -0700

    major changes no progress

--- main.py
+++ main.py
@@ -2,45 +2,50 @@ import pygame
 pygame.init()
 
 SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 900
+SCREEN_HEIGHT = 900 
+left = 100
+right = 500
+top = 100
+bottom = 860
+x = 140
+y = 140
+l = 40
 tickrate = 120
 tick = 0
-x = 100
-y = 100
-l = 40
+
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+gamespace = pygame.Rect((left,top,right-left,bottom-top))
 font = pygame.font.Font(pygame.font.get_default_font(), 36)
 
-
+block = pygame.Rect((x, y, l, l))
 
 run = True
-while run: 
-
+while run:
     clock.tick(tickrate)
     screen.fill((204, 204, 204))
-    pygame.draw.line(screen, (0,0,0), (100,100), (100, 860) )
-    pygame.draw.line(screen, (0,0,0), (500,100), (500, 860) )
-    pygame.draw.line(screen, (0,0,0), (100, 860), (500, 860) )
-    pygame.draw.line(screen, (0,0,0), (100,100), (500, 100) )
     
     tick+=1
     if tick == tickrate/2: 
-        y = y+l
+        block.y+=l
         tick = 0
+    
+    pygame.draw.rect(screen, (0,0,0), gamespace)
+    pygame.draw.rect(screen, (0, 50, 100), block)
 
-    pygame.draw.rect(screen, (0, 50, 100), (x, y, l, l))
+    if pygame.Rect.colliderect(gamespace, block):
+        run == False
+    
     text_surface = font.render('X:'+str(x)+" Y:"+str(y)+" FPS:"+str(round(clock.get_fps())), True, (0, 0, 0))
-
 
     for event in pygame.event.get():
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT and x>100:
-                x -= l
+                block.x -= l
             elif event.key==pygame.K_RIGHT and x+l<500:
-                x += l
-        if event.type == pygame.QUIT:
-            run = False
+                block.x += l
+            elif event.key == pygame.K_ESCAPE:
+                run = False
     
     screen.blit(text_surface, dest=(0,0))
     pygame.display.flip()
